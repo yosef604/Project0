75ef5dafafdc5d7deb384699c4f16a3cd6a7924f
Yosef Levy <yoseflevy@Yosefs-MacBook-Pro.local>
2021-09-02 16:32:25 +0300

    header n footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
-function App() {
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <h1>hello</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
